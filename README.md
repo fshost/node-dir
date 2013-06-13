13044dba5d4a554ba87d8163c6242677d00863be
Nathan Cartwright <fshost@yahoo.com>
2013-06-13 18:08:51 -0500

    converted tests to use mocha/chai and updated the readme

--- README.md
+++ README.md
@@ -1,62 +1,64 @@
 # node-dir
-small node.js module to provide some convenience methods for asynchronous, non-blocking recursive directory operations like being able to get an array of all files, subdirectories, or both (with the option to either combine or separate the results), and for sequentially reading and processing the contents all files in a directory recursively, optionally firing a callback when finished.
+A small node.js module to provide some convenience methods for asynchronous, non-blocking recursive directory operations,including methods to get an array of all files, subdirectories, or both (with the option to either combine or separate the results), and a method for sequentially reading and processing the contents all files in a directory recursively, optionally firing a callback when finished.
 
-####methods
+### methods
 for the sake of brevity, assume that the following line of code precedes all of the examples
 
     var dir = require('node-dir');
 
+and that callbacks would typically have a first line of code something like
 
-####readFiles( dir, fileCallback, finishedCallback )
+	if (err) throw err;
+        
+#### readFiles( dir, fileCallback, finishedCallback )
 sequentially read the content of each file in a directory, passing the contents to a callback, optionally calling a finished callback when complete.
 
-    dir.readFiles(__dirname, 
-        function(err, content, next) {
-            if (err) throw err;
-            console.log(content);
-            // required to continue to next file:
+    dir.readFiles(__dirname,
+    	function(err, content, filepath, next) {
+            console.log('filepath:', filepath + '\n' + 'content:', content);
             next();
-    	},
-    	function(err, files){
-        	if (err) throw err;
-        	console.log('finished reading files:',files);
+        },
+        function(err, files){
+        	console.log('finished reading files:', files);
         });
 
-		
-####files( dir, callback )
+#### files( dir, callback )
 asynchronously iterate the files of a directory and its subdirectories and pass an array of file paths to a callback
+
 	dir.files(__dirname, function(err, files) {
-	    if (err) throw err;
-	    console.log(files);
+		console.log(files);
 	});
 
 		
-####subdirs( dir, callback )
+#### subdirs( dir, callback )
 asynchronously iterate the subdirectories of a directory and its subdirectories and pass an array of directory paths to a callback
 
 	dir.subdirs(__dirname, function(err, subdirs) {
-		if (err) throw err;
 		console.log(subdirs);
 	});
 		
 
-####paths(dir, [combine], callback )
+#### paths(dir, [combine], callback )
 asynchronously iterate the subdirectories of a directory and its subdirectories and pass an array of both file and directory paths to a callback
 
-separated into two distinct arrays (paths.files and paths.dirs)
+###### separated into two distinct arrays (paths.files and paths.dirs)
 
 		dir.paths(__dirname, function(err, paths) {
-			if (err) throw err;
 			console.log('files:\n',paths.files);
 			console.log('subdirs:\n', paths.dirs);
 		});
 		
-combined in a single array (convenience method for concatenation of the above)
+###### combined in a single array (convenience method for concatenation of the above)
 
 		dir.paths(__dirname, true, function(err, paths) {
-			if (err) throw err;
 			console.log('paths:\n',paths);
 		});
-		
-## License
-MIT licensed (SEE LICENSE.txt)
+
+### tests
+to run the tests, you will need mocha, chai, and should.js installed, preferably globally, in which case you can run the tests by simply typing
+	
+	mocha
+
+
+### License
+MIT-Style licensed. See LICENSE.txt.
--- test/test.js
+++ test/test.js
@@ -1,98 +1,119 @@
-﻿var path = require('path'),
-	assert = require('assert'),
-	dir = require('..'),
-	tdir = __dirname + '/testdir';
+﻿var path = require('path');
 
-// TODO: assert callback arguments to be as expected from test data
+var chai = require('chai');
+var expect = chai.expect;
+chai.should();
 
-/*
+var dir = require('..'),
+    tdir = __dirname + '/testdir';
 
-console.log('\n');
+var filenames = [];
 
-console.log('test exports.paths:');
-exports.paths('../test/testdir', function (err, paths) {
-	if (err) throw err;
-	console.log('files:', paths.files);
-	console.log('subdirs:', paths.dirs);
-	
-
-	console.log('\ntest exports.paths (combined):');
-	exports.paths('../test/testdir', true, function (err, paths) {
-		if (err) throw err;
-		console.log('paths:',paths);
-		
-		console.log('\n');
-exports.subdirs('../test/testdir', function(err, dirs) { console.log(dirs); });
-console.log('\n');
-exports.files('../test/testdir', function(err, files) { console.log(files); });
-	});
+describe('readfiles method', function() {
+    it('should exec a callback on every file contents and exec a done callback', function(done) {
+        dir.readFiles(
+            tdir, function(err, content, filename, next) {
+            expect(err).to.equal(null);
+            content = content.replace(/\r/g, '');
+            var shortName = path.basename(filename).replace(new RegExp(path.extname(filename) + '$'), '');
+            var expected = 'begin content of ' + shortName + '\ncontent body\nend content of ' + shortName;
+            filenames.push(shortName);
+            content.should.equal(expected);
+            next();
+        }, function(err, files) {
+            expect(err).to.equal(null);
+            var relFiles = files.map(function(curPath) {
+                return path.relative(__dirname, curPath);
+            });
+            relFiles.should.eql([
+                    'testdir/file1.txt',
+                    'testdir/file2.txt',
+                    'testdir/subdir/file3.txt',
+                    'testdir/subdir/file4.txt'
+            ]);
+            filenames.should.eql(['file1', 'file2', 'file3', 'file4']);
+            done();
+        });
+    });
 });
-*/
 
+describe('readFiles method', function() {
+    it('should pass the name and content of every filepath in a directory and subdirectories to a callback, and pass an array of the filepaths of every file in a directory and its subdirectories to a callback when complete', function(done) {
+        dir.files(tdir, function(err, files) {
+            expect(err).to.equal(null);
+            var relFiles = files.map(function(curPath) {
+                return path.relative(__dirname, curPath);
+            });
+            relFiles.should.eql([
+                    'testdir/file1.txt',
+                    'testdir/file2.txt',
+                    'testdir/subdir/file3.txt',
+                    'testdir/subdir/file4.txt'
+            ]);
+            done();
+        });
+    });
+});
 
-console.log('\n### test reading contents of all files in dir and subdirs ###\n');
+describe('subdirs method', function() {
+    it('should pass an array of the subdir paths of every subdir in a directory (recursive) to a callback', function(done) {
+        dir.subdirs(tdir, function(err, dirs) {
+            expect(err).to.equal(null);
+            var relPaths = dirs.map(function(curPath) {
+                return path.relative(__dirname, curPath);
+            });
+            relPaths.length.should.equal(1);
+            relPaths[0].should.equal('testdir/subdir');
+            done();
+        });
 
-dir.readFiles(tdir,
-    function(err, content, next) {
-	
-		console.log('\t## executing callback for file content ##\n');
-        if (err) throw err;
-		content = content.split('\n').map(function(val) { return '\t\t' + val; }).join('');
-        console.log(content,'\n');
-        next();
-		
-    },
-    function(err, files){
-	
-		console.log('\n\t## executing readFiles complete callback ##\n');
-		
-        if (err) throw err;
-        files.forEach(function(val) { console.log('\t\t',val); });
-		
-		console.log('\n\n### test getting all subdir paths ###\n');
-		
-		dir.subdirs(tdir, function(err, dirs) {
-		
-			if (err) throw err;
-			dirs.forEach(function(val) { console.log('\t',val); });
-			
-			console.log('\n\n### test getting all file paths ###\n');
-			
-			dir.files(tdir, function(err, files) {
-			
-				if (err) throw err;
-				files.forEach(function(val) { console.log('\t',val); });
-				
-				console.log('\n\n### test getting all subdirs and files ###\n');
-				
-				dir.paths(tdir, function(err, paths) {
-				
-					if (err) throw err;
-					console.log('\tsubdirs:');
-					paths.dirs.forEach(function(val) { console.log('\t\t',val); });
-					console.log('\tfiles:');
-					paths.files.forEach(function(val) { console.log('\t\t',val); });
-					
-					console.log('\n\n### test getting all subdirs and files (combined) ###\n');
-					dir.paths(tdir, true, function(err, paths) {
-					
-						if (err) throw err;
-						
-						console.log('\tpaths:');
-						paths.forEach(function(val) { console.log('\t\t',val); });
-						console.log('\n\n### test getting all subdirs and files (combined) ###\n');
-						console.log('\n\ntests complete!\n');
-							
-					});
-					
-				});
-				
-				
-			});
-			
-			
-		});
+    });
+});
 
-		
-    }
-);
+describe('paths method', function() {
+    it('should pass an object with a files property and dirs property of the paths of every file and subdir, respectively, in a directory (recursive) to a callback', function(done) {
+        dir.paths(tdir, function(err, paths) {
+            expect(err).to.equal(null);
+            paths.should.be.an('object');
+            paths.should.not.be.an('array');
+            expect(paths.files).to.exist;
+            expect(paths.dirs).to.exist;
+            var relFiles = paths.files.map(function(curPath) {
+                return path.relative(__dirname, curPath);
+            });
+            var relPaths = paths.dirs.map(function(curPath) {
+                return path.relative(__dirname, curPath);
+            });
+            relFiles.should.eql([
+                'testdir/file1.txt',
+                'testdir/file2.txt',
+                'testdir/subdir/file3.txt',
+                'testdir/subdir/file4.txt'
+            ]);
+            relPaths.length.should.equal(1);
+            relPaths[0].should.equal('testdir/subdir');
+            done();
+        });
+    });
+
+    describe('when called with combine argument set to true', function() {
+        it(' should pass an array of filepaths of all subdirs and files in a directory and its subdirs to a callback', function(done) {
+            dir.paths(tdir, true, function(err, paths) {
+                expect(err).to.equal(null);
+                paths.should.be.an('array');
+                var relPaths = paths.map(function(curPath) {
+                    return path.relative(__dirname, curPath);
+                });
+                relPaths.should.eql([
+                    'testdir/file1.txt',
+                    'testdir/file2.txt',
+                    'testdir/subdir/file3.txt',
+                    'testdir/subdir/file4.txt',
+                    'testdir/subdir'
+                ]);
+            });
+            done();
+        });
+    });
+
+});
